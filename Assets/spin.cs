48e0de00fcb1db4e3da79d7b9f5c5d07143a72b3
littotimmy <littotimmy@gmail.com>
2018-03-20 16:24:22 +0530

    first commit

---
+++ Assets/spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class spin : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
